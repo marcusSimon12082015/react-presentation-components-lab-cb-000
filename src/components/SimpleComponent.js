18c07e4b0e4b3ef8e669db54c0a0dcc6c73bcb1b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 18:03:26 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,15 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      mood:'happy'
+    }
+  }
+
+  render(){
+    return(<div>{this.state.mood}</div>);
+  }
+}
