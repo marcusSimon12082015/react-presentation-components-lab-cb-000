374d3925c49b9f1e0fc835ad50b59162b0653ad5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 18:12:33 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 
 const SimplerComponent = (props) => (
-  <div onClick={this.props.handleClick}>I am just happy</div>
+  <div onClick={props.handleClick}>I am just happy</div>
 )
 export default SimplerComponent;
